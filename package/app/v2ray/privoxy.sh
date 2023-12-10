59032448ff7239f014dceab14ba3a6066a3332ec
再遇已是幽都 <46696929+zaiyuyishiyoudu@users.noreply.github.com>
2023-12-11 00:38:30 +0800

    Update privoxy.sh

--- package/app/v2ray/privoxy.sh
+++ package/app/v2ray/privoxy.sh
@@ -40,7 +40,7 @@ sudo /etc/init.d/privoxy restart
 
 sudo /etc/init.d/squid restart
 
-git clone https://github.com/tsl0922/ttyd.git
+git clone -b 1.6.0 https://github.com/tsl0922/ttyd.git
 
 cd ttyd && mkdir build && cd build
 
