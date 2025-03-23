5e99697f8fa72e824d1d11a58563f415a2797de0
再遇已是幽都 <46696929+zaiyuyishiyoudu@users.noreply.github.com>
2025-03-23 20:29:58 +0800

    Update privoxy.sh

--- package/app/v2ray/privoxy.sh
+++ package/app/v2ray/privoxy.sh
@@ -16,7 +16,7 @@ sudo /etc/init.d/zerotier-one start
 ##sudo zerotier-one & 2>null && sleep 3 && 
 sudo zerotier-cli join d3ecf5726d2307a9
 
-echo '----------------Hello World-----------------1'
+echo '----------------Hello World-----------------'
 
 sleep 40
 
